a32eceedeb6e61db02da225d9313167f348319a0
yjjqtd <qifengfeng2011@163.com>
2017-02-17 17:46:44 +0800

    Update reflect.go

--- reflect.go
+++ reflect.go
@@ -13,8 +13,7 @@ func (this *MyStruct) GetName() string {
 	return this.name
 }
 /*
-  TypeOf用来返回变量的类型，ValueOf用来返回变量的方法。
-  我们在初始化一个结构体的时候，有两种方式，var a Struct跟a :=new(Struct)，其中，前者的类型是struct，后者的类型是指针。
+
 */
 func main() {
 	a := new(MyStruct)
@@ -25,10 +24,35 @@ func main() {
 	var b MyStruct
 	b.name = "abc"
 	fmt.Println(reflect.TypeOf(b))
+	
+	var c MyStruct	
+	c.name = "xiangli"
+	fmt.Println(reflect.ValueOf(c).FieldByName("name").CanSet()) //false
+	fmt.Println(reflect.ValueOf(&(c.name)).Elem().CanSet())      //true
+
+	fmt.Println("--------------")
+	var str string = "yejianfeng"
+	p := reflect.ValueOf(&str)
+	fmt.Println(p.CanSet())        //false
+	fmt.Println(p.Elem().CanSet()) //true
+	p.Elem().SetString("newName")
+	fmt.Println(str)
 }
 /*
   输出结果
   *main.MyStruct
   -------------------
   main.MyStruct
+*/
+/*
+1、TypeOf用来返回变量的类型，ValueOf用来返回变量的方法。
+我们在初始化一个结构体的时候，有两种方式，var a Struct跟a :=new(Struct)，其中，前者的类型是struct，后者的类型是指针。
+
+2、reflect.ValueOf(a).FieldByName方法
+如果a是结构体，reflect.ValueOf(a).FieldByName("name")等价于a.name。
+如果是指针的话ValueOf返回的是指针的Type，它是没有Field的，所以也就不能使用FieldByName
+
+3、CanSet方法
+CanSet当Value是可寻址的时候，返回true，否则返回false。
+当前面的CanSet是一个指针的时候（p）它是不可寻址的，但是当是p.Elem()(实际上就是*p)，它就是可以寻址的
 */
